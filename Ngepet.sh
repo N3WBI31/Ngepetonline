d87c5146d39e79c31837ad531265fabd30c98edf
RelGanz <105323573+N3WBI31@users.noreply.github.com>
2022-05-15 20:23:48 +0700

    Add files via upload

---
+++ Ngepet.sh
@@ -0,0 +1,50 @@
+ clear
+ bi='\033[34;1m' #biru
+ i='\033[32;1m' #ijo
+ pur='\033[35;1m' #purple
+ cy='\033[36;1m' #cyan
+ me='\033[31;1m' #merah
+ pu='\033[37;1m' #putih
+ ku='\033[33;1m' #kuning
+ mer='\033[41;97m' #Tepi
+ st='\033[0m' #Stop
+ 
+ 
+echo $pur"      (\____/)    " 
+echo $pur"      / @__@ \ "
+echo $pur"     (  (oo)  )  ╔╗╔╔═╗╔═╗╔═╗╔═╗╔╦╗  " 
+echo $pur"      \-.~~.-/   ║║║║ ╦║╣ ╠═╝║╣  ║    " 
+echo $pur"       /    \    ╝╚╝╚═╝╚═╝╩  ╚═╝ ╩   "       
+echo $pur"     @/      \_ "
+echo $pur"    (/ /    \ \)"
+echo $pur"     WW\----/WW"
+echo $pur" Author : RelGanz [ MR.BksMilenial ]"
+echo ""
+echo -n $cy"Masukkan Nama Daerah Ngepet Kalian : "
+read Daerah
+echo -n $cy"Masukkan Jumlah Target Uang : "
+read uang
+echo
+echo -n $cy"Masukkan No Rek Lu : "
+read norek
+echo -n $cy"Masukkan Nama Rekening : "
+read nama
+echo
+clear
+echo "load.." | lolcat
+echo "..." | lolcat
+echo "...." | lolcat
+echo "......." | lolcat
+echo "..........." | lolcat
+echo "................. SUKSES/100%" | lolcat
+echo "wait.............." | lolcat 
+clear
+sleep 3
+echo "|=====================================|" | lolcat
+echo "| TRANSAKSI Hasil Ngepet Onlen SUKSES!|" | lolcat
+echo "|=====================================|" | lolcat
+echo 
+echo "$nama, Uang sejumlah $uang"
+echo "Telah sukses di Transfer ke Nomer Rek Lu, Silakan Di Cek Kembali" | lolcat
+
+echo " Cari Duit Yang Halal GOBLOK Jangan Ngepet BABI!!" | lolcat
